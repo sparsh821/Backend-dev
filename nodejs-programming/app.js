1be4719879690fb5f6a62812b15400a8465a684f
sparsh-dev <sparsh.saxena_cs23@gla.ac.in>
2026-01-22 14:40:28 +0530

    Delete nodejs-programming directory

--- nodejs-programming/app.js
+++
@@ -1,5 +0,0 @@
-const stringUtils = require("./stringUtils");
-
-console.log(stringUtils.capitalize("hello"));
-console.log(stringUtils.reverse("nodejs"));
-console.log(stringUtils.countVowels("JavaScript"));
