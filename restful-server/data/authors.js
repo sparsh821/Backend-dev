491bea3a12e78fc836fd60d8d077e510e2945370
sparsh-dev <sparsh.saxena_cs23@gla.ac.in>
2026-03-12 10:43:57 +0530

    Add files via upload

---
+++ restful-server/data/authors.js
@@ -0,0 +1,7 @@
+
+let authors = [
+ { id:1, name:"John" },
+ { id:2, name:"Mike" }
+];
+
+module.exports = authors;
---
+++ restful-server/data/books.js
@@ -0,0 +1,8 @@
+
+let books = [
+ { id:1, title:"Node Basics", author:"John", year:2020 },
+ { id:2, title:"Express Guide", author:"Mike", year:2021 },
+ { id:3, title:"MongoDB Mastery", author:"John", year:2019 }
+];
+
+module.exports = books;
